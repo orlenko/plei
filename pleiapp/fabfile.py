ca628c770735259a1d253936b54f0c7cff80cc69
Vlad Orlenko <vlad@bjola.ca>
2013-06-03 06:01:32 -0700

    Updating fabfile

--- pleiapp/fabfile.py
+++ pleiapp/fabfile.py
@@ -36,6 +36,7 @@ env.user = conf.get("SSH_USER", getuser())
 env.password = conf.get("SSH_PASS", None)
 env.key_filename = conf.get("SSH_KEY_PATH", None)
 env.hosts = conf.get("HOSTS", [])
+env.port = conf.get("SSH_PORT", 22)
 
 env.proj_name = conf.get("PROJECT_NAME", os.getcwd().split(os.sep)[-1])
 env.venv_home = conf.get("VIRTUALENV_HOME", "/home/%s" % env.user)
