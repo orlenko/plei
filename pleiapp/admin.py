6ea3cf32e228d97e418977933359e9e2473580df
Vlad Orlenko <vlad@bjola.ca>
2013-06-11 07:18:58 -0700

    registered type, topic and category in admin

--- pleiapp/admin.py
+++ pleiapp/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from mezzanine.pages.admin import PageAdmin
 
-from .models import Resource
+from .models import Type, Category, Topic, Resource
 
+admin.site.register(Type, PageAdmin)
+admin.site.register(Category, PageAdmin)
+admin.site.register(Topic, PageAdmin)
 admin.site.register(Resource, PageAdmin)
